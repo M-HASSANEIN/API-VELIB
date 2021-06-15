b57d01cd834e884e5e685558b2f8b785107b5e07
mohamed hassanein <momed999@hotmail.com>
2021-06-15 15:28:23 +0200

    update

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ function alertContents() {
 
             if (map == null) {
                 // create the map
-                map = L.map('map').setView([result.position.lat, result.position.lng], 15);
+                map = L.map('map').setView([result.position.lat, result.position.lng], 20);
                 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
                     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                     maxZoom: 18,
